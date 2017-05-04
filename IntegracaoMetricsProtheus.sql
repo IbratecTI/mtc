17f815eca5bf9a429be8a23b13e9279c7a4be994
Fernando Lima <fernando.lima@ibratecgrafica.com.br>
2017-05-04 17:41:26 -0300

    Inserindo o Job para execução da Rotina de Integração

--- IntegracaoMetricsProtheus.sql
+++ IntegracaoMetricsProtheus.sql
@@ -886,7 +886,6 @@ GO
 -- Description:	Carregas as informações sobre as alterações na tabela de Cabeçalho de Notas Fiscais de Saída e Entrada
 --				Metrics "NotasFiscais, EstNotasFiscaisEntrada"  para a tabela de integração Local "Usr_ZZ5010"
 -- =============================================
-
 -- Drop stored procedure if it already exists
 IF EXISTS (SELECT *
            FROM   INFORMATION_SCHEMA.ROUTINES
@@ -1441,7 +1440,6 @@ GO
 --          	"ItemNota e ESTItemNotaEntrada" para a tabela de integração Local "Usr_ZZ3010"
 -- =============================================
 -- Drop stored procedure if it already exists
-
 IF EXISTS (SELECT *
            FROM   INFORMATION_SCHEMA.ROUTINES
            WHERE  SPECIFIC_SCHEMA = N'[dbo].[Usr_SP_IntegraItensNotas]'
@@ -5009,4 +5007,225 @@ AS
         DEALLOCATE CursorDI
     END Catch
 
-GO 
+GO
+
+USE [msdb]
+
+GO
+
+/****** Object:  Operator [Fernando Lima]    Script Date: 04/05/2017 17:28:26 ******/
+EXEC msdb.dbo.Sp_add_operator
+  @name=N'Fernando Lima',
+  @enabled=1,
+  @weekday_pager_start_time=90000,
+  @weekday_pager_end_time=180000,
+  @saturday_pager_start_time=90000,
+  @saturday_pager_end_time=180000,
+  @sunday_pager_start_time=90000,
+  @sunday_pager_end_time=180000,
+  @pager_days=0,
+  @email_address=N'fernando.lima@ibratecgrafica.com.br',
+  @category_name=N'[Uncategorized]'
+
+GO
+
+USE [msdb]
+
+GO
+
+/****** Object:  Operator [Rene Francisco]    Script Date: 04/05/2017 17:28:35 ******/
+EXEC msdb.dbo.Sp_add_operator
+  @name=N'Rene Francisco',
+  @enabled=1,
+  @weekday_pager_start_time=90000,
+  @weekday_pager_end_time=180000,
+  @saturday_pager_start_time=90000,
+  @saturday_pager_end_time=180000,
+  @sunday_pager_start_time=90000,
+  @sunday_pager_end_time=180000,
+  @pager_days=0,
+  @email_address=N'rene.francisco@ibratecgrafica.com.br',
+  @category_name=N'[Uncategorized]'
+
+GO
+
+-- =============================================
+-- Author:		Fernando Lima
+-- Create date: 04/05/2017
+-- Description:	Job de Integração Metrics Protheus 
+-- =============================================
+USE [msdb]
+
+USE [msdb]
+GO
+
+IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs_view WHERE name = N'Integração Metrics Protheus')
+EXEC msdb.dbo.sp_delete_job @job_name=N'Integração Metrics Protheus', @delete_unused_schedule=1
+GO
+
+/****** Object:  Job [Integração Metrics Protheus]    Script Date: 04/05/2017 17:38:12 ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 04/05/2017 17:38:12 ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Integração Metrics Protheus', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'Integração da tabela de Itens de Estoque do Metrics com a tabela de cadastro de Produtos do Protheus', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', 
+		@notify_email_operator_name=N'Fernando Lima', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Atualiza as tabelas de Cadastro de Clientes e Fornecedores do Protheus - ZZ1010]    Script Date: 04/05/2017 17:38:12 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Atualiza as tabelas de Cadastro de Clientes e Fornecedores do Protheus - ZZ1010', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=3, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'DECLARE	@return_value int
+
+EXEC	@return_value = [dbo].[USR_SP_IntegraCliFor_Protheus]
+
+SELECT	''Return Value'' = @return_value', 
+		@database_name=N'MT_Ibratec', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Atualiza as tabelas de Cadastro de Produtos do Protheus - ZZ4010]    Script Date: 04/05/2017 17:38:12 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Atualiza as tabelas de Cadastro de Produtos do Protheus - ZZ4010', 
+		@step_id=2, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=3, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'DECLARE	@return_value int
+
+EXEC	@return_value = [dbo].[USR_SP_IntegraProdutos_Protheus]
+
+SELECT	''Return Value'' = @return_value', 
+		@database_name=N'MT_Ibratec', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Atualiza as tabelas de Notas Fiscais do Protheus - ZZ5010]    Script Date: 04/05/2017 17:38:12 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Atualiza as tabelas de Notas Fiscais do Protheus - ZZ5010', 
+		@step_id=3, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'DECLARE	@return_value int
+
+EXEC	@return_value = [dbo].[Usr_SP_IntegraNotas_Protheus]
+
+SELECT	''Return Value'' = @return_value', 
+		@database_name=N'MT_Ibratec', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Atualiza as tabelas de  Item Notas Fiscais do Protheus - ZZ3010]    Script Date: 04/05/2017 17:38:12 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Atualiza as tabelas de  Item Notas Fiscais do Protheus - ZZ3010', 
+		@step_id=4, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'DECLARE	@return_value int
+
+EXEC	@return_value = [dbo].[Usr_sp_integraitensnotas_protheus] 
+
+SELECT	''Return Value'' = @return_value', 
+		@database_name=N'MT_Ibratec', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Atualiza as tabelas de Rateios Financeiro das Notas de Compras ZR6010]    Script Date: 04/05/2017 17:38:12 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Atualiza as tabelas de Rateios Financeiro das Notas de Compras ZR6010', 
+		@step_id=5, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'DECLARE	@return_value int
+
+EXEC	@return_value = [dbo].[Usr_sp_integrarateiofin_protheus]
+
+SELECT	''Return Value'' = @return_value', 
+		@database_name=N'MT_Ibratec', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Atualiza as tabelas de Declarações de Importação das Notas Fiscais ZI5010]    Script Date: 04/05/2017 17:38:12 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Atualiza as tabelas de Declarações de Importação das Notas Fiscais ZI5010', 
+		@step_id=6, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'DECLARE	@return_value int
+
+EXEC	@return_value = [Usr_sp_integradi_protheus]
+
+SELECT	''Return Value'' = @return_value', 
+		@database_name=N'MT_Ibratec', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 2
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Todos os dias, a cada 5 minutos', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20170504, 
+		@active_end_date=99991231, 
+		@active_start_time=120000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'67743043-c9f5-419e-96bd-c26057e22047'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+
+GO
+
+
